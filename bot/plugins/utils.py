22414395b769df317392f13aee35d40877478794
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-04-12 22:59:03 +0530

    Update utils.py

--- bot/plugins/utils.py
+++ bot/plugins/utils.py
@@ -5,7 +5,8 @@ from time import sleep
 from sys import executable
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, RPCError
-from bot.config import SUDO_USERS, DOWNLOAD_DIRECTORY, LOGGER
+from bot.config import SUDO_USERS, DOWNLOAD_DIRECTORY
+from bot import LOGGER
 
 
 @Client.on_message(filters.private & filters.incoming & filters.command(['log']) & filters.user(SUDO_USERS), group=2)
