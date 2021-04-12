380138dcefaac8aa7677109f0ef538264a6892ca
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-04-12 20:23:24 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,13 +1,4 @@
-class Config:
-    BOT_TOKEN = os.environ.get('BOT_TOKEN')
-    APP_ID = os.environ.get('APP_ID')
-    API_HASH = os.environ.get('API_HASH')
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    SUDO_USERS = os.environ.get('SUDO_USERS')
-    DOWNLOAD_DIRECTORY = os.environ.get("DOWNLOAD_DIRECTORY", "./downloads/")
-    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", "568fguijfdry.goohle.com")
-    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", "FxvbXbjuyr357e34fcj8")
-
+import os
 
 class BotCommands:
   Download = ['download', 'dl']
