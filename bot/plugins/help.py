97a5f8ac67146fee3fba4356b93a540c11d49c90
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-04-12 19:00:53 +0530

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -70,3 +70,4 @@ def _start(client, message):
     client.send_message(chat_id = message.chat.id,
         text=tr.UPDATE_MSG.format(message.from_user.first_name),
         disable_web_page_preview=True
+     )
