918465dee7d545d7b6ca2fe3343676b612c26679
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-04-12 23:31:40 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -21,7 +21,7 @@ class Config:
     G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", "FxvbXbjuyr357e34fcj8")
     
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "").split())
 
 
 class BotCommands:
