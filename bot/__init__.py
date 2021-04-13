5af59e88e244181f47b3838447dd003a19d7de4d
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-04-13 07:00:37 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,31 +1,6 @@
 import os
 import logging
 
-class Config:
-
-    # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-
-    # The Telegram API things
-    # Get these values from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
-
-     # Sql Database url
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
-
-    # the download location, where the HTTP Server runs
-    DOWNLOAD_DIRECTORY = "./DOWNLOADS"
-
-    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", "")
-
-    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", "")
-  
-    SUDO_USERS = "754495556"
-    
-    SUPPORT_CHAT_LINK = "https://t.me/HxSupport"
-    
-
 
 logging.basicConfig(
     level=logging.INFO,
