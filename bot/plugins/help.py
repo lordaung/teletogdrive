ed9ffd0f16cbe97a5aeec23b80a84dd7b15feb0a
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-04-13 08:40:48 +0530

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -12,37 +12,6 @@ from ..config import Config
 from bot import LOGGER
 
 
-@Client.on_message(
-     filters.private 
-     & filters.incoming 
-     & filters.command('log') 
-     & filters.user(Config.AUTH_USERS), group=2
-)
-
-def _send_log(client, message):
-  with open('log.txt', 'rb') as f:
-    try:
-      client.send_document(
-        message.chat.id,
-        document=f,
-        file_name=f.name,
-        reply_to_message_id=message.message_id
-        )
-      LOGGER.info(f'Log file sent to {message.from_user.id}')
-    except FloodWait as e:
-      sleep(e.x)
-    except RPCError as e:
-      message.reply_text(e, quote=True)
-
-@Client.on_message(filters.private & filters.incoming & filters.command(['restart']) & filters.user(Config.SUDO_USERS), group=2)
-def _restart(client, message):
-  shutil.rmtree(Config.DOWNLOAD_DIRECTORY)
-  LOGGER.info('Deleted Config.DOWNLOAD_DIRECTORY successfully.')
-  message.reply_text('**♻️Restarted Successfully !**', quote=True)
-  LOGGER.info(f'{message.from_user.id}: Restarting...')
-  execl(executable, executable, "-m", "bot")
-
-
 @Client.on_message(filters.private & filters.incoming & filters.command(['start']), group=2)
 def _start(client, message):
     client.send_message(chat_id = message.chat.id,
