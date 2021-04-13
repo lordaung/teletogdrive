c6f3e07972b8d1254693c845519c24847147787a
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-04-13 08:43:47 +0530

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -62,7 +62,7 @@ def map(pos):
 
         button = [
             [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")],
-            [InlineKeyboardButton(text = '-->', callback_data = f"help+{pos+1}")]
+             InlineKeyboardButton(text = '-->', callback_data = f"help+{pos+1}")
 
         ]
     else:
